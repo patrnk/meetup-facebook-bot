4176c16fd0f7bd9448d987ae6f51a6361f006334
mcproger <artem7lomonosov@gmail.com>
2017-04-26 22:24:23 +0300

    Add database migrate

---
+++ database_migrate/versions/change_scripts.py
@@ -0,0 +1,12 @@
+from sqlalchemy import *
+from migrate import *
+
+
+def upgrade(migrate_engine):
+    #Upgrades coming soon
+    pass
+
+
+def downgrade(migrate_engine):
+    #Downgrades coming soon
+    pass
