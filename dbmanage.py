bdf2bbdafeadac3170396466fb99e2b19a347a32
mcproger <artem7lomonosov@gmail.com>
2017-04-26 22:29:52 +0300

    Add database migrate

--- dbmanage.py
+++ dbmanage.py
@@ -2,4 +2,4 @@
 from migrate.versioning.shell import main
 
 if __name__ == '__main__':
-    main(repository='database_migrate', debug='False', url='sqlite:////home/mcproger/meetup-facebook-bot/app.db')
+    main(repository='database_migrate', debug='False', url='sqlite:////home/mcproger/meetup-facebook-bot/app.db') #for testing on local machine
