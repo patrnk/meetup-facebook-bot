58d08c9fff5a4fba98dc7765806a3af3c91144c1
mcproger <artem7lomonosov@gmail.com>
2017-04-27 21:46:52 +0300

    Add example of script to create a new table

--- database_migrate/versions/001_change_scripts.py
+++ database_migrate/versions/001_change_scripts.py
@@ -1,12 +1,20 @@
-from sqlalchemy import *
-from migrate import *
+from sqlalchemy import Table, Column, Integer, String, MetaData
+
+meta = MetaData()
+
+account = Table(
+    'account', meta,
+    Column('id', Integer, primary_key=True),
+    Column('login', String(40)),
+    Column('passwd', String(40)),
+)
 
 
 def upgrade(migrate_engine):
-    #Upgrades coming soon
-    pass
+    meta.bind = migrate_engine
+    account.create()
 
 
 def downgrade(migrate_engine):
-    #Downgrades coming soon
-    pass
+    meta.bind = migrate_engine
+    account.drop()
