ba395732195931ec315ffc489f50fd686477192e
mcproger <artem7lomonosov@gmail.com>
2017-05-04 19:55:36 +0300

    Fix database migrate

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 
 import os
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:////home/mcproger/meetup-facebook-bot/app.db' #os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']  
 
-ACCESS_TOKEN = 'EAAFumq2b8TMBAFpAcL1rS1fYWElrr1ZBAbCAyVqjIK0TmoSeCPTZAHkUNr2H4VEiZAK28uCfMaLMoRFep4d2KyT9tQuqWyUPiNvSJpzmJvSQ5lOwoZBZCOfTPsTsaV96CHJLeWKibkTCVMcbZALttvBCeAyLnmKICr2UQlWTKkwOvyx22Ktwj3'    #os.environ['ACCESS_TOKEN']
-PAGE_ID = '12345678899' #os.environ['PAGE_ID']
-APP_ID = '12375467667'  #os.environ['APP_ID']
-VERIFY_TOKEN = '1245466867978'      #os.environ['VERIFY_TOKEN']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']     
+PAGE_ID = os.environ['PAGE_ID']   
+APP_ID = os.environ['APP_ID']   
+VERIFY_TOKEN = os.environ['VERIFY_TOKEN']      
