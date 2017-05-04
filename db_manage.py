66216f9620b3ba4d80968b1c8ea6f8a290a2a15a
mcproger <artem7lomonosov@gmail.com>
2017-05-04 19:57:06 +0300

    Rename database_manage.py

--- db_manage.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from migrate.versioning.shell import main
-import os
-
-if __name__ == '__main__':
-    main(repository='database_migrate', debug='False', url='sqlite:////home/mcproger/meetup-facebook-bot/app.db') 
