22919d5c03d496f333d7dd4681695c0e633e3bd9
Dmitry Konyshev <dvkonyshev@edu.hse.ru>
2017-05-08 18:18:14 +0300

    Add flask forms

--- meetup_facebook_bot/server.py
+++ meetup_facebook_bot/server.py
@@ -110,7 +110,7 @@ def is_facebook_challenge_request(request):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    user_ip = request.headers['X-Forwarded-For'].split[','][0]
+    user_ip = request.headers['X-Forwarded-For'].split(',')[0]
     if form.validate(user_ip):
         logged[user_ip] = True
         flash('Successfully logged in')
